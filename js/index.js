fb025ba6f6f2944abc4e681c62f9dd94cca18648
Juliana Carvalho <146263453+JulianaCarvalho16@users.noreply.github.com>
2023-12-15 18:30:51 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botoes = document.querySelectorAll(".botoes .botao");
+    const personagens = document.querySelectorAll(".personagem");
+
+    botoes.forEach((botao, index) => {
+        botao.addEventListener("click", function () {
+            
+            botoes.forEach((b) => b.classList.remove("selecionado"));
+            
+            botao.classList.add("selecionado");
+
+            
+            personagens.forEach((personagem) => personagem.style.display = "none");
+
+           
+            personagens[index].style.display = "block";
+        });
+    });
+});
